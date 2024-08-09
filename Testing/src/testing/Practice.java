aee5c4a3860f05ce539d285ecc8bbf3ac64a4d42
siddh <siddh@Saanvi>
2024-08-09 20:00:04 +0530

    First Commit

---
+++ Testing/src/testing/Practice.java
@@ -0,0 +1,15 @@
+package testing;
+
+import org.testng.annotations.Test;
+
+public class Practice {
+	
+	@Test
+	public void testing()
+	{
+		System.out.println("siddhant is working on testn");
+	}
+
+}
+
+
